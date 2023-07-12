eba31d4d4ec0278bde41fd0793b45c365f9b4575
Jay Liu <jayliu89@gmail.com>
2023-07-11 18:00:19 -0700

    added comments to javascript

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-currentHour = dayjs().format("H")
+var currentHour = dayjs().format("H")
 
 $(function () {
   
+  //created an event listener to each button on the daily planner. Each button will only listen to itself
+  //and save the task from the button's parent timeblock
   $(".saveBtn").on("click", function (){
     var tasks = $(this).siblings(".description").val()
     var taskKey = $(this).parent().attr("id")
-    // console.log($(this).parent())
     localStorage.setItem(taskKey, tasks)
   })
 
+  // Loop through each timeblock and compare it to the current hour and then add an
+  // appropriate time class based on the current time vs the timeblock's hour
   for (var i = 9; i < 18; i++) {
     var textarea = $(`#hour-${i}`); 
     if (currentHour==i) {
@@ -20,6 +23,8 @@ $(function () {
     }
   }
 
+  // on application load, check local storage for anything, and if there are any saved tasks, append them to 
+  // their relative timeblocks
   for(var i = 9; i < 18; i++) {
   var getTask = localStorage.getItem(`hour-${i}`)
   var getHour = $(`#hour-${i}`).children(".description")
